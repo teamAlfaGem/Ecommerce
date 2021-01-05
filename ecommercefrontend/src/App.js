7268ae09aa9a8ad10867b25c0b3ab902f7820b44
raavinduf <ravinduf@rocketmail.com>
2021-01-05 11:17:53 +0530

    fixed routing issues and added two layouts

--- ecommercefrontend/src/App.js
+++ ecommercefrontend/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
-import NavBar from './components/NavBar/NavBar'
+
 import Home from './components/Home/Home'
 import Cart from './components/Cart/Cart'
 import SignIn from './components/Users/SignIn/SignIn'
@@ -10,20 +10,38 @@ import Profile from './components/Users/Profile/Profile'
 import SingleProductInfo from './components/Products/SingleProductInfo/SingleProductInfo'
 import Products from './components/Products/Products'
 
+import PageLayout from './Layouts/PageLayout'
+import UsersLayout from './Layouts/UsersLayout'
+
 function App() {
   return (
     <div>
-      <NavBar />
       <Router>
         <Switch>
-          <Route exact path="/"  component={Home}/>
-          <Route exact path="/users/sign-in" component={SignIn}/>
-          <Route exact path="/users/sign-up" component={SignUp}/>
-          <Route exact path="/users/profile" component={Profile}/>
-          <Route exact path="/products/:id" component={Products}/>
-          <Route exact path="/products" component={Products}/>
-          <Route exact path="/products/single-product/:id" component={SingleProductInfo}/>
-          <Route exact path="/cart" component={Cart}/>
+          
+          <Route path="/users/:path" exact>
+            <UsersLayout>
+              <Switch>
+                <Route  path="/users/sign-in" component={SignIn}/>
+                <Route  path="/users/sign-up" component={SignUp}/>
+                <Route  path="/users/profile" component={Profile}/>
+              </Switch>
+            </UsersLayout>
+          </Route>
+
+          <Route>
+            <PageLayout>
+              <Switch>
+                <Route path="/"  component={Home}/>
+                <Route path="/products/:id" component={Products}/>
+                <Route path="/products" component={Products}/>
+                <Route path="/products/single-product/:id" component={SingleProductInfo}/>
+                <Route path="/cart" component={Cart}/>
+
+              </Switch>
+            </PageLayout>
+          </Route>
+
         </Switch>
       </Router>
     </div>
@@ -31,4 +49,11 @@ function App() {
 }
 
 export default App;
+
+
+          
+          
+        
+
+
 
