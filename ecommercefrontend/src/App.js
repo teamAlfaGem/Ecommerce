52a1f656236a787205e5d1bb502d39abe13a97ac
raavinduf <ravinduf@rocketmail.com>
2021-01-05 09:12:13 +0530

    added route to SingleProductInfo

--- ecommercefrontend/src/App.js
+++ ecommercefrontend/src/App.js
@@ -6,18 +6,19 @@ import Cart from './components/Cart/Cart'
 import SignIn from './components/Users/SignIn/SignIn'
 import SignUp from './components/Users/SignUp/SignUp'
 import Profile from './components/Users/Profile/Profile'
+import SingleProductInfo from './components/Products/SingleProductInfo/SingleProductInfo'
 
 function App() {
   return (
     <div>
-      <h2>Hello</h2>
+      
       <Router>
         <Switch>
           <Route exact path="/"  component={Home}/>
           <Route exact path="/users/sign-in" component={SignIn}/>
           <Route exact path="/users/sign-up" component={SignUp}/>
           <Route exact path="/users/profile" component={Profile}/>
-          <Route exact path="/products/:id" />
+          <Route exact path="/products/:id" component={SingleProductInfo}/>
           <Route exact path="/cart" component={Cart}/>
         </Switch>
       </Router>
