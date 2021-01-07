b571d2fb7542289f5d02fdd5d9e7e4545670b31a
raavinduf <ravinduf@rocketmail.com>
2021-01-07 08:51:03 +0530

    added basic styling for Header component used in UserLayout and created style file for header

--- ecommercefrontend/src/components/Header/Header.js
+++ ecommercefrontend/src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+
+import style from './style'
+
 const Header = () => {
+    const classes = style
     return (
         <div>
-            <h1>E-Shop</h1>
+            <Container className="bg-light" fluid>
+                <h1 style={classes.logo} className="display-3">E-Shop</h1>
+            </Container>
         </div>
     )
 }
