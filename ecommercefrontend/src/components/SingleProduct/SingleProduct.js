2b0a92f3452be1767c928e5b203ef048ea1b8b3d
raavinduf <ravinduf@rocketmail.com>
2021-01-10 08:28:31 +0530

    created basic card component to show products

--- ecommercefrontend/src/components/SingleProduct/SingleProduct.js
+++ ecommercefrontend/src/components/SingleProduct/SingleProduct.js
@@ -1,9 +1,24 @@
 import React from 'react'
 
+import Card from 'react-bootstrap/Card'
+import Button from 'react-bootstrap/Button'
+
 const SingleProduct = () => {
     return (
         <div>
-            SingleProduct
+           <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src="holder.js/100px180" />
+                <Card.Body>
+                    <Card.Title>Product Name</Card.Title>
+                    <Card.Text>
+                    Basic description <br/>
+                    of the product<br/>
+                    lorem ipsium
+                    
+                    </Card.Text>
+                    <Button variant="danger">Add to Cart</Button><Button variant="info">View</Button>
+                </Card.Body>
+                </Card>
         </div>
     )
 }
--- ecommercefrontend/src/pages/Products/Products.js
+++ ecommercefrontend/src/pages/Products/Products.js
@@ -1,10 +1,13 @@
 import React from 'react'
 
+import SingleProduct from '../../components/SingleProduct/SingleProduct'
+
 const Products = (props) => {
     return (
         <div>
             {props.match.params.id}<br/>
             Products
+            <SingleProduct />
         </div>
     )
 }
