1ee2855b140d6551ab0880a95fb65c69697a1b9a
raavinduf <ravinduf@rocketmail.com>
2021-01-10 08:41:04 +0530

    added basic styling for product components showing in products page

--- ecommercefrontend/src/pages/Products/Products.js
+++ ecommercefrontend/src/pages/Products/Products.js
@@ -1,15 +1,37 @@
 import React from 'react'
 
+import Container from 'react-bootstrap/Container'
+
 import SingleProduct from '../../components/SingleProduct/SingleProduct'
 
 const Products = (props) => {
     return (
         <div>
             {props.match.params.id}<br/>
-            Products
-            <SingleProduct />
+                Products
+            <Container className="d-flex flex-row flex-wrap justify-content-around">
+                <div className="mb-5">
+                    <SingleProduct />
+                </div>
+                <div>
+                    <SingleProduct />
+                </div>
+                <div>
+                    <SingleProduct />
+                </div>
+                <div>
+                    <SingleProduct />
+                </div>
+                <div>
+                    <SingleProduct />
+                </div>
+                <div>
+                    <SingleProduct />
+                </div>
+            </Container>
         </div>
     )
 }
 
 export default Products
+
