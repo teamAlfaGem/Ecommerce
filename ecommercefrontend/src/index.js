594260036dd4c4b92aa10abe2bbbf278bc332ac5
raavinduf <ravinduf@rocketmail.com>
2021-01-11 09:43:00 +0530

    basic boilerplate code for redux added

--- ecommercefrontend/src/index.js
+++ ecommercefrontend/src/index.js
@@ -3,9 +3,19 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import reducers from './reducers'
+
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware, compose} from 'redux'
+import thunk from 'redux-thunk';
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -14,3 +24,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
