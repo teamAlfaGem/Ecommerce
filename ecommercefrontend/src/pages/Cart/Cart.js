95bb6cead418cd669ed596a3a2562e345b98e0fe
raavinduf <ravinduf@rocketmail.com>
2021-01-11 10:35:18 +0530

    basic card layout done

--- ecommercefrontend/src/pages/Cart/Cart.js
+++ ecommercefrontend/src/pages/Cart/Cart.js
@@ -1,15 +1,46 @@
 import React from 'react'
 
 import Container from 'react-bootstrap/Container'
+import Button from 'react-bootstrap/Button'
 
 import SingleProductInCart from '../../components/SingleProductInCart/SingleProductInCart'
 
+import style from './style'
+
 const Cart = () => {
+    const classes = style
     return (
         <Container>
             Cart
+            <Button style={classes.buyBtn} variant="success" className="mt-3">Buy</Button>
             <h1>Total Price: </h1>
-            <SingleProductInCart />
+            <div className="d-flex flex-wrap flex-row ">
+                <div className="m-3">
+                    <SingleProductInCart />
+                </div>
+                <div className="m-3">
+                    <SingleProductInCart />
+                </div>
+                <div className="m-3">
+                    <SingleProductInCart />
+                </div>
+                <div className="m-3">
+                    <SingleProductInCart />
+                </div>
+                <div className="m-3">
+                    <SingleProductInCart />
+                </div>
+                <div className="m-3">
+                    <SingleProductInCart />
+                </div>
+                <div className="m-3">
+                    <SingleProductInCart />
+                </div>
+
+                
+            </div>
+
+           
         </Container>
     )
 }
---
+++ ecommercefrontend/src/pages/Cart/style.js
@@ -0,0 +1,9 @@
+const style = {
+    buyBtn : {
+        marginRight: '0px',
+        float: 'right',
+        width: '10%'
+    }
+}
+
+export default style
