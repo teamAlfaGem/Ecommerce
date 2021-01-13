acc34035f9ca784daaefbc3d7f53a0e93b84f199
raavinduf <ravinduf@rocketmail.com>
2021-01-13 19:43:36 +0530

    changed the location of the login, logout, register functions

--- ecommercefrontend/src/services/user.service.js
+++ ecommercefrontend/src/services/user.service.js
@@ -1,36 +1,3 @@
-import axios from 'axios'
 
-export const login = async (username, password) => {
-
-    try {
-
-        const response = await axios.post(`${process.env.REACT_APP_API_AUTH}/signin`, {username, password})
-        
-        if(response.data.accessToken) {
-            localStorage.setItem("user", JSON.stringify(response.data))
-        }
-
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-export const logout = () => {
-    localStorage.removeItem("user")
-}
-
-export const register = async (username, email, password) => {
-    
-    try {
-        const response = await axios.post(`${process.env.REACT_APP_API_AUTH}/signup`, {
-            username,
-            email,
-            password
-        })
-
-    } catch (error) {
-        console.log(error)
-    }
-}
     
         
