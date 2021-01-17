cebe09255d77ab9c485c46ddbb47d99281b6a275
raavinduf <ravinduf@rocketmail.com>
2021-01-17 08:42:42 +0530

    removed unneccassary import

--- ecommercefrontend/src/index.js
+++ ecommercefrontend/src/index.js
@@ -7,7 +7,7 @@ import "react-toastify/dist/ReactToastify.css";
 import reducers from './reducers'
 
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware, compose} from 'redux'
+import { createStore, applyMiddleware} from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 import thunk from 'redux-thunk';
