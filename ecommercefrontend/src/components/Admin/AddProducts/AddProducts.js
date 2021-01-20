f85208e649f7f35d5415713c2d4936334fa8ee17
raavinduf <ravinduf@rocketmail.com>
2021-01-20 16:00:43 +0530

    created basic form to add products

--- ecommercefrontend/src/components/Admin/AddProducts/AddProducts.js
+++ ecommercefrontend/src/components/Admin/AddProducts/AddProducts.js
@@ -1,9 +1,60 @@
 import React from 'react'
 
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+
 const AddProducts = () => {
     return (
         <div>
             AddProducts
+            <Form>
+                <Form.Group>
+                    <Form.Label>Product Name</Form.Label>
+                    <Form.Control type="text" />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>Category</Form.Label>
+                    <Form.Control as="select">
+                    <option>Phones</option>
+                    <option>Laptops</option>
+                    <option>Pen drives</option>
+                    <option>Tv</option>
+                    <option>Consoles</option>
+                    </Form.Control>
+                </Form.Group>
+                
+                <Form.Group>
+                    <Form.Label>Brand</Form.Label>
+                    <Form.Control type="text" />
+                </Form.Group>
+                
+                <Form.Group>
+                    <Form.Label>Price</Form.Label>
+                    <Form.Control type="number" />
+                </Form.Group>
+                
+                <Form.Group >
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+                
+                <Form.Group>
+                    <Form.Label>Keyword</Form.Label>
+                    <Form.Control type="text" />
+                    <Form.Text className="text-muted mb-1">
+                        Keywords should be seperated with a space and a #                      
+                    </Form.Text>
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.File  label="Add Image" />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
