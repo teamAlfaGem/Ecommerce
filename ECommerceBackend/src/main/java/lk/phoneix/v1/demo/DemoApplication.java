ad5339de990ecc6ace867d81465f2687287214d8
RavinduDon <gunarath_se17015@stu.kln.ac.lk>
2021-01-22 17:55:12 +0530

    enpoint added

--- ECommerceBackend/src/main/java/lk/phoneix/v1/demo/DemoApplication.java
+++ ECommerceBackend/src/main/java/lk/phoneix/v1/demo/DemoApplication.java
@@ -2,8 +2,10 @@ package lk.phoneix.v1.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties
 public class DemoApplication {
 
     public static void main(String[] args) {
--- ECommerceBackend/src/main/java/lk/phoneix/v1/demo/security/WebSecurityConfig.java
+++ ECommerceBackend/src/main/java/lk/phoneix/v1/demo/security/WebSecurityConfig.java
@@ -53,7 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
+                .antMatchers("/api/product/**").permitAll()
+                .antMatchers("/api/cart/**").permitAll()
                 .anyRequest().authenticated();
         http.addFilterBefore(authJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
