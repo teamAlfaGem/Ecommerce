b98fc13af347edffdaeaef0d77df6846fbae83a7
raavinduf <ravinduf@rocketmail.com>
2021-01-26 14:12:57 +0530

    added a border to admin navabar logo

--- ecommercefrontend/src/components/Admin/AdminNavbar/AdminNavbar.js
+++ ecommercefrontend/src/components/Admin/AdminNavbar/AdminNavbar.js
@@ -9,7 +9,9 @@ import './style.css'
 const AdminNavbar = () => {
     return (
         <Col md={2} className="menu d-flex flex-column text-center">
-            <h1 className="admin-logo">E-Shop</h1>
+            <div className="logo-border pb-2">
+                <h1 className="admin-logo">E-Shop</h1>
+            </div>
            <Nav.Link className="link" href="/admin/home">Home</Nav.Link>
            <Nav.Link className="link" href="/admin/products">Products</Nav.Link>
            <Nav.Link className="link" href="/admin/orders">Orders</Nav.Link>
