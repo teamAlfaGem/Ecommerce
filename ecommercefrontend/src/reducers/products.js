e8069847144a5eab39bf1842c9cbdf6d39ecade1
raavinduf <ravinduf@rocketmail.com>
2021-01-29 19:49:20 +0530

    updated

--- ecommercefrontend/src/reducers/products.js
+++ ecommercefrontend/src/reducers/products.js
@@ -5,7 +5,6 @@ const initialState = [];
 export const products = (state = initialState, action) => {
     switch (action.type) {
         case GET_PRODUCTS:
-            console.log('hello')
             return [...state, action.payload]
         default:
             return state;
