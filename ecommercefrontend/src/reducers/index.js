12fd107db5d350874e8c7f5f3e4a9d4bc08982cb
raavinduf <ravinduf@rocketmail.com>
2021-01-29 19:41:26 +0530

    created reducer products and added to root reducer

--- ecommercefrontend/src/reducers/index.js
+++ ecommercefrontend/src/reducers/index.js
@@ -3,9 +3,11 @@ import { combineReducers } from 'redux'
 import { cartProducts } from './cart'
 import auth from './auth'
 import message from './message'
+import { products }  from './products'
 
 export default combineReducers({
     cartProducts,
     auth,
-    message
+    message,
+    products
 })
