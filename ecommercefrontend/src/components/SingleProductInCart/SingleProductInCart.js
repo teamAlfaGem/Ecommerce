3b78be9de6ff80997b5ac82afa3703b99ed2adfe
raavinduf <ravinduf@rocketmail.com>
2021-02-05 20:06:46 +0530

    added deleteFromCart function

--- ecommercefrontend/src/components/SingleProductInCart/SingleProductInCart.js
+++ ecommercefrontend/src/components/SingleProductInCart/SingleProductInCart.js
@@ -3,8 +3,43 @@ import React from 'react'
 import Card from 'react-bootstrap/Card'
 import Button from 'react-bootstrap/Button'
 
+import axios from 'axios';
+
 
 const SingleProductInCart = ({product}) => {
+
+   
+    const updateQty = (amount) => {
+        
+        try {
+            const newQty = product.qty + amount;
+
+            axios.put(`${process.env.REACT_APP_API}/cart/updatecart`, {
+                id: product.cartId,
+                userId: JSON.parse(sessionStorage.getItem('user')).id,
+                productId: product.productId,
+                qty: newQty
+            })
+
+            window.location.reload()
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    
+
+    const removeFromCart = (cartId) => {
+
+        try{
+
+            axios.delete(`${process.env.REACT_APP_API}/cart/delete/${cartId}`);
+            window.location.reload()
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }
     return (
         <div>
             <Card style={{ width: '18rem' }}>
@@ -12,10 +47,17 @@ const SingleProductInCart = ({product}) => {
                 <Card.Body className="">
                     <Card.Title>{product.productName}</Card.Title>
                     <Card.Text>
+                    
                     Price : {product.price}<br/><br/>
-                    Qty:&nbsp; &nbsp; <Button variant="warning" size="sm">+</Button>&nbsp;{product.qty}&nbsp;<Button variant="warning" size="sm">-</Button>
+
+                    Qty:&nbsp; &nbsp;   <Button variant="warning" size="sm" onClick={() => {updateQty(1)}}>+</Button>
+                                            &nbsp;{product.qty}&nbsp;
+                                        <Button variant="warning" size="sm" onClick={() => {updateQty(-1)}}>-</Button>
                     </Card.Text>
-                    <Button variant="dark" block>Remove from cart</Button>
+                    
+                    <Button variant="dark" block onClick={() => {removeFromCart(product.cartId)}}>
+                        Remove from cart
+                    </Button>
                 </Card.Body>
                 </Card>
         </div>
