60da8dc821a2193d68725185cec1ca206cc86802
raavinduf <ravinduf@rocketmail.com>
2021-02-06 20:24:58 +0530

    updated cart.js reducer to get cart products

--- ecommercefrontend/src/reducers/cart.js
+++ ecommercefrontend/src/reducers/cart.js
@@ -5,13 +5,17 @@ import { GET_CART_PRODUCTS, REMOVE_FROM_CART_PRODUCTS, INCREMENT_QTY_CART_PRODUC
 export const cartProducts = (products = [], action) => {
     switch (action.type) {
         case GET_CART_PRODUCTS:
-            return products
+            return action.payload
+
         case REMOVE_FROM_CART_PRODUCTS:
             return products
+
         case INCREMENT_QTY_CART_PRODUCTS:
             return products
+
         case DECREMENT_QTY_CART_PRODUCTS:
             return products
+            
         default:
             return products;
     }
