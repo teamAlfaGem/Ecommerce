c3c3baed6f815a6a3fb459fa9c2766191b7c1ab1
raavinduf <ravinduf@rocketmail.com>
2021-02-07 20:01:53 +0530

    created basic form

--- ecommercefrontend/src/pages/Purchase/Purchase.js
+++ ecommercefrontend/src/pages/Purchase/Purchase.js
@@ -1,9 +1,48 @@
 import React from 'react'
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 const Purchase = () => {
     return (
         <div>
             <h1>Purchase page</h1>
+
+            <Form>
+                <Form.Group>
+                    <Form.Label>Address</Form.Label>
+                    <Form.Control type="text" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group>
+                    <Form.Label>City</Form.Label>
+                    <Form.Control type="text" placeholder="Password" />
+                </Form.Group>
+                
+                <Form.Group>
+                    <Form.Label>Zip</Form.Label>
+                    <Form.Control type="text" placeholder="Password" />
+                </Form.Group>
+                
+                <Form.Group>
+                    <Form.Label>Card Name</Form.Label>
+                    <Form.Control type="text" placeholder="Password" />
+                </Form.Group>
+                
+                <Form.Group>
+                    <Form.Label>Expire date</Form.Label>
+                    <Form.Control type="date" placeholder="Password" />
+                </Form.Group>
+                
+                <Form.Group>
+                    <Form.Label>Expire date</Form.Label>
+                    <Form.Control type="date" placeholder="Password" />
+                </Form.Group>
+                
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
         </div>
     )
 }
